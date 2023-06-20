258fcd9ff6464635c2ba8b02da7c28a7464b14c1
silverwind <me@silverwind.io>
2023-06-20 22:36:11 +0200

    use vitest-config-silverwind

--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,4 @@
 import {defineConfig} from "vitest/dist/config.js";
+import {config} from "vitest-config-silverwind";
 
-export default defineConfig({
-  test: {
-    environment: "node",
-    testTimeout: 20000,
-    open: false,
-    allowOnly: true,
-    passWithNoTests: true,
-    globals: true,
-    watch: false,
-    outputDiffLines: Infinity,
-  },
-});
+export default defineConfig(config);
